5d0c168c1af2e24e290b6bbbfc4e29705ba958b1
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2019-12-23 21:08:55 +0100

    Use the new logging methods

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ import os, sys, urllib, urllib2, logging
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(cur_dir, "VS-Utils"))
 from parse import parse_cfg
+from prints import debugmsg, errmsg
 
 ### Synoindex-Client
 def client(source_host, output_host=None):
@@ -15,15 +16,6 @@ def client(source_host, output_host=None):
 	config_file = os.path.join(cur_dir, "config.txt")
 	cfg = parse_cfg(config_file, "vs-synoindex", "docker")
 
-	## Setup the client logging file
-	client_log = "%s/%s" % (cfg.client_logs, "client.log")
-	logging.basicConfig(filename=client_log, filemode='a',
-						format='%(asctime)s - %(levelname)s: %(message)s')
-
-	## Set the logger and its level
-	logger = logging.getLogger(__name__)
-	logger.setLevel(logging.DEBUG)
-
 	## Call the url and get the answer of the server
 	if not output_host:
 		query_vars = {'source_host': source_host}
@@ -31,6 +23,6 @@ def client(source_host, output_host=None):
 		query_vars = {'source_host': source_host, 'output_host': output_host}
 	url = "%s/synoindex?" % cfg.url
 	url = url + urllib.urlencode(query_vars)
-	logger.debug(url)
+	debugmsg("Sent to SynoIndex-Server")
 	contents = urllib2.urlopen(url).read()
-	logger.debug(contents)
+	debugmsg("SynoIndex-Server answered with", (contents,))
